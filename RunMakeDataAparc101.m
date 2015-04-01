e7267032833a9f9da572b5d46c8ec5efc7452408
Estrid <estrid@gmail.com>
2015-04-01 10:00:31 +0200

    added comments and renamed variables

--- RunMakeDataAparc101.m
+++ RunMakeDataAparc101.m
@@ -1,3 +1,7 @@
+%runs MakeDataAparc script for 101 HCP subjects
+%saves matrix with connectivity data (features) and a vector of broca label data (labels) separately
+%NOTE: connectivity file is HUGE (15GB)! Don't try to open on a normal computer.
+
 subject_list = importdata(['/scr/murg2/HCP_Q3_glyphsets_left-only/subject_list_101.txt']);
 
 Data = [];
@@ -14,4 +18,4 @@ end
 labels = Data(32493,:);
 features = Data(1:32492,:);
 
-save('BrocaDataAparc_new.mat', '-v7.3', 'labels', 'features');
+save('BrocaDataAparc.mat', '-v7.3', 'labels', 'features');
