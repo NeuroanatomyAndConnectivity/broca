2c2148983c68f5173f4e3e40170c1c17e3d12223
Estrid <estrid@gmail.com>
2015-04-29 14:25:03 +0200

    updates and option to subselect data

--- broca_classifier.m
+++ broca_classifier.m
@@ -1,25 +1,47 @@
+% % if running matlab 8.5:
+%addpath(genpath('/a/software/matlab/8.5/toolbox/stats/'));
+
 %run decision tree classification with 10-fold cross-validation
-load /scr/murg2/MachineLearning/BrocaData_new.mat
-tree = ClassificationTree.fit(features', labels', 'crossval', 'on')
-view(tree.Trained{1}, 'Mode', 'graph')
 
-%import novel dataset and mask to freesurfer vlpfc label
-AnatLabels = gifti(['/afs/cbs.mpg.de/documents/connectome/_all/984472/MNINonLinear/fsaverage_LR32k/984472.L.aparc.32k_fs_LR.label.gii']);
-AnatLabelsData = AnatLabels.cdata;
-op = AnatLabelsData == 18;
-tri = AnatLabelsData == 20;
-vlpfc = op + tri;
-fid = fopen(['/scr/murg2/HCP500_glyphsets/984472/rfMRI_REST_left_corr_avg.gii.data'], 'r');
+% load connectivity data features, 44 & 45 labels, and op & tri freesurfer labels
+data = load('/scr/murg2/MachineLearning/BrocaDataAparc.mat');
+
+% subselect data only for speed purposes
+sel = [1:1000:size(data.features,2)]; % number of training sets
+% sel_feat = [1:100:size(data.features,1)]; % number of conn features
+
+features = [data.op(sel); data.tri(sel); data.features(:,sel)];
+labels = data.labels(1,sel);
+features = features';
+labels = labels';
+
+% run classification training
+tree = ClassificationTree.fit(features, labels, 'crossval', 'on');
+% For matlab 8.5 
+% tree = fitctree(features, labels,'CrossVal','on');
+% output cross-validation loss for each fold
+L = kfoldLoss(tree,'mode','individual')
+% visualize decision tree
+view(tree.Trained{1}, 'Mode', 'graph');
+
+%test classifier on a novel dataset
+% import novel dataset
+fid = fopen(['/scr/murg2/HCP500_glyphsets/994273/rfMRI_REST_left_corr_avg.gii.data'], 'r');
 M = fread(fid,[32492 32492], 'float32');
-M_masked = M(find(vlpfc),:);
+% get freesurfer labels for new data
+AnatLabels = gifti(['/scr/murg2/HCP500_glyphsets/994273/994273.L.aparc.32k_fs_LR.label.gii']);
+AnatLabelsData = AnatLabels.cdata;
+op_new = AnatLabelsData == 18;
+tri_new = AnatLabelsData == 20;
+op_new = op_new';
+tri_new = tri_new';
+data_new = [op_new; tri_new; M];
+data_new = data_new';
 
-%predict labels for novel dataset
-label = predict(tree.Trained{1},M_masked);
-
-%save results for visualization
-results = zeros([length(vlpfc) 1]);
-results(find(vlpfc)) = label;
-filename = ['984472_results.1D'];
+% predict labels for novel dataset
+label = predict(tree.Trained{1},data_new);
+% save results for visualization
+filename = ['/scr/murg2/MachineLearning/994273_results.1D'];
 fid = fopen(filename,'w');
-fprintf(fid, '%u\n', results);
+fprintf(fid, '%u\n', label);
 fclose(fid);
