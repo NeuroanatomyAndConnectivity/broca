f8dd37f0962b1f46960b7e5b716cb418cc7131d8
Estrid <estrid@gmail.com>
2016-05-18 14:08:04 +0200

    reads in gifti files and indexes correctly

--- PartialCorrelation/ICA_NKI.py
+++ PartialCorrelation/ICA_NKI.py
@@ -5,8 +5,7 @@ from nilearn.decomposition.canica import CanICA
 from nibabel import gifti
 
 # LEFT indices
-img = nib.load('/a/documents/connectome/_all/100307/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
-cort = img.header.matrix.mims[1].brainModels[0].vertexIndices.indices
+cort = nib.freesurfer.read_label('/afs/cbs.mpg.de/software/freesurfer/5.3.0/ubuntu-precise-amd64/subjects/fsaverage5/label/lh.cortex.label')
 
 # load data
 #subs = ['A00028185'] # list of subject ids to include
@@ -15,7 +14,9 @@ filenames = []
 for sub in subs:
     print "SUBJECT ", sub
     try:
-        data = gifti.giftiio.read('/scr/ilz1/nilearn_vol2surf_sink/fwhm6/%s_lh_preprocessed_fsaverage5_fwhm6.gii' % sub)
+        data_file = gifti.giftiio.read('/scr/ilz1/nilearn_vol2surf_sink/fwhm6/%s_lh_preprocessed_fsaverage5_fwhm6.gii' % sub)
+        data = np.array([data_file.darrays[i].data for i in range(len(data_file.darrays))])
+        data = data.T
         data = data[cort, :]
         data = np.expand_dims(data,axis=1)    
         data = np.expand_dims(data,axis=1)
@@ -25,20 +26,10 @@ for sub in subs:
         filenames.append(filename)
     except:
         print "subject " + sub + " cannot be run"
-         
-    # load data:                
-    # data = nib.load('/scr/murg2/HCP_new/HCP_Q1-Q6_GroupAvg_Related440_Unrelated100_v1/HCP_Q1-Q6_R468_rfMRI_groupPCA_d4500_Eigenmaps_MGTR_left_metric_smoothed2.dtseries.nii')
-    # d = data.get_data().squeeze()
-    # cort = np.where(d.sum(axis=0) != 0.)[0]
-    # d = d[:,cort]
-    # d = np.expand_dims(d.T,axis=1)
-    #d = np.expand_dims(d,axis=1)
-    #filename = '%s.nii.gz' % sub
-    #img = nib.Nifti1Image(d, np.eye(4))
-    #img.to_filename(filename)
+        
 
 # create artificial mask:
-mask = np.ones(29696)
+mask = np.ones(len(cort))
 mask = np.expand_dims(mask,axis=1)
 mask = np.expand_dims(mask,axis=1)
 img = nib.Nifti1Image(mask, np.eye(4))
@@ -56,13 +47,7 @@ canica.fit(filenames)
 # Retrieve the independent components in brain space
 components_img = canica.masker_.inverse_transform(canica.components_)
 
-A = np.zeros((32492,n_components))
-# STILL NEED TO RESOLVE CORT INDICIES
-
-# LEFT indices
-img = nib.load('/a/documents/connectome/_all/100307/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
-cort = img.header.matrix.mims[1].brainModels[0].vertexIndices.indices
-
+A = np.zeros((10242,n_components))
 A[cort,:] = components_img.get_data().squeeze()
 #np.save('/scr/murg2/MachineLearning/partialcorr/ICA/ICA_HCP/ica_HCP500_output_%s.npy' % str(n_components), A)
 savemat('/scr/murg2/MachineLearning/partialcorr/ICA/ICA_NKI/ica_NKI_output_%s.mat' % str(n_components), {'ic':A})
